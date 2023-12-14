6f8d826ea35f70f12dd7a234a4ab1d0878d5fda9
martinezpke <martinezpke26@gmail.com>
2023-12-14 17:17:48 -0500

    se realizo diseño para el register, pero tambien falta mejoras a errores

--- src/components/FormRegister.jsx
+++ src/components/FormRegister.jsx
@@ -1,86 +1,186 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './FormRegister.css'
-import Cookies from 'js-cookie';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./FormRegister.css";
+import Cookies from "js-cookie";
+import axios from "axios";
 
 function FormRegister() {
   const [formData, setFormData] = useState({
-    username: '',
-    name: '',
-    last_name: '',
-    country: '',
-    email: '',
-    password: ''
+    username: "",
+    name: "",
+    last_name: "",
+    country: "",
+    email: "",
+    password: "",
+    password2: ""
   });
 
-  const [error, setError] = useState(null);
-  const [success, setSuccess] = useState(false);
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [msg, setMsg] = useState();
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
+  };
+
+  const handleSuccess = (response) => {
+    const token = response.data.token;
+    if (token) {
+      Cookies.set("token", token);
+      navigate("/test");
+      console.log("Registro exitoso:", response.data);
+    }
+  };
+
+  const handleApiError = (error) => {
+    if (axios.isCancel(error)) {
+      console.log("La solicitud fue cancelada");
+    } else if (error.response) {
+      setMsg(error.response.data.msg);
+      console.error("Respuesta de error de la API:", error.response.data);
+    } else if (error.request) {
+      console.error("No se recibió respuesta de la API");
+    } else {
+      console.error("Error durante la solicitud:", error.message);
+    }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
+
+    const abortController = new AbortController();
+    const signal = abortController.signal;
 
     try {
-      const response = await axios.post('http://localhost:8080/sign-up', formData);
-      const token = await response.data.token;
-      Cookies.set('token', token);
-      
-      setSuccess(true);
-      setError()
-      navigate('/test');
-      // Puedes manejar la respuesta de la API según tus necesidades
-      console.log('Registro exitoso:', response.data);
+      const response = await axios.post(
+        "http://localhost:8080/sign-up",
+        formData,
+        { cancelToken: signal.token }
+      );
+      handleSuccess(response);
     } catch (error) {
-      setError(error);
+      handleApiError(error);
+    } finally {
+      setIsLoading(false);
+      abortController.abort();
     }
   };
 
+  const cookieToke = () => {
+    const getToken = Cookies.get("token");
+    if (getToken) {
+      navigate("/test");
+    }
+  };
+
+  useEffect(() => {
+    cookieToke();
+  }, [navigate]);
+
   return (
-    <div className='register-container'>
-      <h1>Registro</h1>
-      {success && <p>Registro exitoso</p>}
-      {error && <p>Error: {error.message}</p>}
-      <form onSubmit={handleSubmit} className='register-form'>
-        <label>
-          Username:
-          <input type="text" name="username" value={formData.username} onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-          Name:
-          <input type="text" name="name" value={formData.name} onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-          Last Name:
-          <input type="text" name="last_name" value={formData.last_name} onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-          Country:
-          <input type="text" name="country" value={formData.country} onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input type="email" name="email" value={formData.email} onChange={handleChange} />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" name="password" value={formData.password} onChange={handleChange} />
-        </label>
-        <br />
-        <button type="submit">Registrar</button>
-      </form>
+    <div className="wrapper">
+      <div className="grip__container">
+        <div className="form-login column">
+          <h1>Registro</h1>
+          {msg && <p className="msg-error">{msg}</p>}
+          <form onSubmit={handleSubmit}>
+            <div className="row-input">
+              <div className="row">
+                <i className="bx bxs-user"></i>
+                <input
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  placeholder="Nombre"
+                />
+              </div>
+              <div className="row">
+                <input
+                  type="text"
+                  name="last_name"
+                  value={formData.last_name}
+                  onChange={handleChange}
+                  placeholder="Apellido"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <i className='bx bxs-user-circle'></i>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                value={formData.username}
+                onChange={handleChange}
+                placeholder="Usuario"
+              />
+            </div>
+
+            <div className="row-input">
+              <div className="row">
+                <i className="bx bxs-envelope"></i>
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Email"
+                />
+              </div>
+
+              <div className="row">
+                {/* <i className="bx bxs-globe"></i> */}
+                <input
+                  type="text"
+                  name="country"
+                  value={formData.country}
+                  onChange={handleChange}
+                  placeholder="País"
+                />
+              </div>
+            </div>
+
+            <div className="row-input">
+              <div className="row">
+                <i className="bx bxs-lock-alt"></i>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  placeholder="Contraseña"
+                />
+              </div>
+
+              <div className="row">
+                {/* <i className="bx bxs-lock-alt"></i> */}
+                <input
+                  type="password"
+                  name="password2"
+                  id="password2"
+                  value={formData.password2}
+                  onChange={handleChange}
+                  placeholder="Contraseña"
+                />
+              </div>
+            </div>
+
+            <div className="btn-sut">
+              <button type="submit" style={{ marginTop: "10px" }}>
+                {isLoading ? "Cargando..." : "Registrar"}
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="background-img"></div>
+      </div>
     </div>
   );
 }
