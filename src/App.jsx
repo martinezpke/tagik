7272e1db8e792c871963f3774a40690959a5b3d2
martinezpke <martinezpke26@gmail.com>
2023-12-20 08:27:35 -0500

    se hicieron modificaciones, se agrego una nueva rita llamada card, modificaciones en el css principal y se agrego una imagen de prueba

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ProtectedView from './pages/ProtectedView';
+import Card from './pages/Card';
 /* import './App.css' */
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
             <Route path="/sign-in" element={<Login />} />
             <Route path="/sign-up" element={<Register />} />
             <Route path="/test" element={<ProtectedView />} />
+            <Route path="/card" element={ <Card /> } />
          </Routes>
       </Router>
   )
